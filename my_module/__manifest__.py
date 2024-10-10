c895643584e82411e4f38b0513b28bb27d0d1a8b
Alejandro Rodríguez González <arodriguez.gonzalez@knowmadmood.com>
2024-10-10 13:33:19 +0200

    Primer commit estructura

---
+++ my_module/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Primer modulo',
+    'version': '1.0',
+    'depends': ['base'],
+    'data': [
+        'views/my_view.xml',
+        'security/ir.model.access.csv',
+    ],
+    'installable': True,
+    'application': True,
+}
